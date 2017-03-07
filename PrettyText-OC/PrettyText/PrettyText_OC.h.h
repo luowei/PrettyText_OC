918bf49bce20d21a1fd00c8fdf9fa6eb494b057b
luowei <luowei03@anjuke.com>
2017-03-07 21:05:44 +0800

    添加统一头文件

---
+++ PrettyText-OC/PrettyText/PrettyText_OC.h.h
@@ -0,0 +1,16 @@
+//
+//  PrettyText_OC.h.h
+//  PrettyText-OC
+//
+//  Created by luowei on 2017/3/7.
+//  Copyright (c) 2017 luowei. All rights reserved.
+//
+
+#ifndef PrettyText_OC_h_h
+#define PrettyText_OC_h_h
+
+#import "LWPrettyTextMaker.h"
+#import "NSMutableAttributedString+PrettyText.h"
+#import "NSString+PrettyText.h"
+
+#endif /* PrettyText_OC_h_h */
